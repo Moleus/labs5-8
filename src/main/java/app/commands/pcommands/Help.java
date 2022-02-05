0b243a9291fdadb18b3d47b278538e75ffa38d16
Moleus <fafufuburr@gmail.com>
2022-02-05 10:36:49 +0300

    Replace all new line characters with system-dependent equivalents.

--- src/main/java/app/commands/pcommands/Help.java
+++ src/main/java/app/commands/pcommands/Help.java
@@ -15,7 +15,7 @@ public final class Help extends AbstractCommand {
   @Override
   public ExecutionResult execute(ExecutionPayload payload) {
     String commandsWithDescriptions = commandManager.getUserAccessibleCommands().values().
-        stream().map(c -> String.format("%28s: %s", c.getName(), c.getDescription())).collect(Collectors.joining("\n"));
+        stream().map(c -> String.format("%28s: %s", c.getName(), c.getDescription())).collect(Collectors.joining(System.lineSeparator()));
     return ExecutionResult.valueOf(true, commandsWithDescriptions);
   }
 }
