Moleus/labs5-8

commit 54b8cf51af6f75fb398150d33371b0624ecef8d6

Fix a bug when first new element is not added to collection if ids in a file are in a reverse order.

src/main/java/app/collection/FlatBuilder.java
@@ -12,10 +12,12 @@ import app.collection.data.View;
 import app.exceptions.InvalidDataValues;
 
 public class FlatBuilder {
-  private Integer nextId;
+  private int nextId;
+  private final List<Integer> usedIds;
   private static FlatBuilder instance;
   private FlatBuilder() {
     nextId = 0;
+    usedIds = new ArrayList<>();
   }
 
   public static FlatBuilder createInstance() {
@@ -38,7 +40,11 @@ public class FlatBuilder {
                     View view,
                     House house
                     ) {
-    nextId = id + 1;
+    do {
+      nextId = id + 1;
+    }
+    while (usedIds.contains(nextId));
+    usedIds.add(id);
     return new Flat(id, name, coordinates, creationDate, area, numberOfRooms, furniture, newness, view, house);
   }
 

commit bc21c5bfbdc255ee7d9b2fd239ccb2e2d0f127de

Fix infinite loop caused by commit 54b8cf5.

src/main/java/app/collection/FlatBuilder.java
@@ -41,9 +41,8 @@ public class FlatBuilder {
                     House house
                     ) {
     do {
-      nextId = id + 1;
-    }
-    while (usedIds.contains(nextId));
+      nextId++;
+    } while (usedIds.contains(nextId));
     usedIds.add(id);
     return new Flat(id, name, coordinates, creationDate, area, numberOfRooms, furniture, newness, view, house);
   }