caabda5cfff8e91b392f0cc9b42670dcdcce9c61
Moleus <fafufuburr@gmail.com>
2022-02-28 09:55:34 +0300

    Separate client only commands

---
+++ shared/src/main/java/commands/pcommands/ExecuteScript.java
@@ -0,0 +1,31 @@
+package commands.pcommands;
+
+import commands.CommandInfo;
+import commands.ExecutionMode;
+import commands.ExecutionPayload;
+import commands.ExecutionResult;
+import commands.AbstractCommand;
+
+import exceptions.ScriptExecutionException;
+import utils.ScriptExecutor;
+
+public final class ExecuteScript extends AbstractCommand {
+  private final ScriptExecutor executor;
+
+  public ExecuteScript(ScriptExecutor executor) {
+    super(CommandInfo.of("execute_script", "Run all commands from specified file", true, 1, false, ExecutionMode.CLIENT));
+    this.executor = executor;
+  }
+  
+  @Override
+  public ExecutionResult execute(ExecutionPayload payload) {
+    String fileName = payload.getInlineArg();
+    try {
+      executor.executeScript(fileName);
+    } catch (ScriptExecutionException e) {
+      return ExecutionResult.valueOf(false, e.getMessage());
+    }
+    return ExecutionResult.valueOf(true, "Script execution started");
+  }
+}
+
