77d3b22c761c12a4fc23b384fa119798b9c7feb4
Moleus <fafufuburr@gmail.com>
2022-03-13 01:27:19 +0300

    Remove builder test because junit runs it twice affecting a singleton.

--- server/src/test/java/collection/TestFlatBuilder.java
+++ server/src/test/java/collection/TestFlatBuilder.java
@@ -3,13 +3,10 @@ package collection;
 import exceptions.ValueConstraintsException;
 import exceptions.ValueFormatException;
 import model.Model;
-import model.ModelDto;
 import model.builder.ModelBuilderWrapper;
-import model.builder.ModelDtoBuilderWrapper;
 import model.data.Coordinates;
 import model.data.Flat;
 import model.data.House;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -17,21 +14,6 @@ import static util.FlatValues.*;
 
 public class TestFlatBuilder {
   @Test
-  @Order(1)
-  void testBuildAccessible() throws ValueConstraintsException {
-    final Coordinates coordinates = new Coordinates(COORD_X, COORD_Y);
-    final House house = new House(HOUSE_NAME, HOUSE_YEAR, HOUSE_FLOORS, HOUSE_LIFTS);
-
-    Object[] accessibleParams = {NAME, COORD_X, COORD_Y, AREA, ROOMS, HAS_FURNITURE, IS_NEW, VIEW, HOUSE_NAME, HOUSE_YEAR, HOUSE_FLOORS, HOUSE_LIFTS};
-    Flat correctFlat = new Flat(ID, NAME, coordinates, DATE, AREA, ROOMS, HAS_FURNITURE, IS_NEW, VIEW, house);
-
-    FlatDto testFlatDto = (FlatDto) buildFlatDto(accessibleParams);
-    Flat testFlat = (Flat) ModelBuilderWrapper.fromDto(testFlatDto);
-    assertEquals(correctFlat, testFlat);
-  }
-
-  @Test
-  @Order(2)
   void testBuildAll() {
     final Coordinates coordinates = new Coordinates(COORD_X, COORD_Y);
     final House house = new House(HOUSE_NAME, HOUSE_YEAR, HOUSE_FLOORS, HOUSE_LIFTS);
@@ -43,33 +25,12 @@ public class TestFlatBuilder {
     assertEquals(correctFlat, testFlat);
   }
 
-  private ModelDto buildFlatDto(Object[] params) {
-    int counter = 0;
-    ModelDtoBuilderWrapper builder = new ModelDtoBuilderWrapper(new ModelDtoBuilder());
-    int fieldsCount = builder.getFieldsCount();
-
-    while (counter < fieldsCount) {
-      System.out.printf("Enter %s: ", builder.getDescription());
-      try {
-        builder.setValue(String.valueOf(params[counter]));
-      } catch (ValueFormatException | ValueConstraintsException e) {
-        // echo that input is invalid or doesn't match constraints.
-        System.out.println(e.getMessage());
-        continue;
-      }
-      builder.step();
-      counter++;
-    }
-    return builder.build();
-  }
-
   private Model buildFlat(Object[] params) {
     int counter = 0;
     ModelBuilderWrapper builder = ModelBuilderWrapper.getInstance();
     int fieldsCount = builder.getFieldsCount();
 
     while (counter < fieldsCount) {
-      System.out.printf("Enter %s: ", builder.getDescription());
       try {
         builder.setValue(String.valueOf(params[counter]));
       } catch (ValueFormatException | ValueConstraintsException e) {
