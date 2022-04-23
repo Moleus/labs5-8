14fd8fef8c293758550f3d31d4def617f64ed2c2
Moleus <fafufuburr@gmail.com>
2022-04-23 17:43:20 +0300

    [Add] repository specific exceptions

--- perfORM/src/main/java/perform/exception/InvalidRepositoryException.java
+++ perfORM/src/main/java/perform/exception/InvalidRepositoryException.java
@@ -2,7 +2,7 @@ package perform.exception;
 
 public class InvalidRepositoryException extends PerformException {
   public InvalidRepositoryException(String message, Class<?> repoType) {
-    super(message + " (In repository [" + repoType.getSimpleName() + "]");
+    super(message + " In repository [" + repoType.getSimpleName() + "]");
   }
 
   public InvalidRepositoryException(String message) {
