e6751417b69f39571c72094826e96acbcb74f2d1
Moleus <fafufuburr@gmail.com>
2022-04-25 10:03:30 +0300

    [Del] unused getRecursive method in ClassUtil.

--- perfORM/src/main/java/perform/util/ClassUtil.java
+++ perfORM/src/main/java/perform/util/ClassUtil.java
@@ -1,11 +1,6 @@
 package perform.util;
 
-import perform.annotations.Collectible;
-
-import java.lang.reflect.Field;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class ClassUtil {
@@ -20,18 +15,6 @@ public class ClassUtil {
     boxedToPrimitive.put(Integer.class, int.class);
     boxedToPrimitive.put(Long.class, long.class);
     boxedToPrimitive.put(Short.class, short.class);
-  }
-
-  public static List<Field> getRecursiveFields(Class<?> clazz) {
-    List<Field> recursiveFields = new ArrayList<>();
-    for (Field field : clazz.getDeclaredFields()) {
-      Class<?> fieldType = field.getType();
-      if (fieldType.getAnnotation(Collectible.class) != null) {
-        recursiveFields.addAll(getRecursiveFields(fieldType));
-      }
-      recursiveFields.add(field);
-    }
-    return recursiveFields;
   }
 
   public static Class<?> resolveToPrimitive(Class<?> boxedType) {
