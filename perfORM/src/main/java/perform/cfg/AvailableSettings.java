30f06a4bf5999144e9c3c2869f437f00c27f7456
Moleus <fafufuburr@gmail.com>
2022-04-25 09:28:25 +0300

    [Fix] a typo in AvailableSettings variable name.

--- perfORM/src/main/java/perform/cfg/AvailableSettings.java
+++ perfORM/src/main/java/perform/cfg/AvailableSettings.java
@@ -4,6 +4,6 @@ public interface AvailableSettings {
   String URL = "perform.connection.url";
   String USER = "perform.connection.user";
   String PASS = "perform.connection.password";
-  String CONNECTION_PREFFIX = "perform.connection";
+  String CONNECTION_PREFIX = "perform.connection";
   String DRIVER = "perform.connection.driver_class";
 }
