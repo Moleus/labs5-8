9cc508ca23b01e6da264deb7a619df21a6c42065
Moleus <fafufuburr@gmail.com>
2022-04-26 18:01:16 +0300

    [Add] Implement User validation and registration.

---
+++ server/src/main/java/server/authentication/UserManager.java
@@ -0,0 +1,36 @@
+package server.authentication;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import server.generated.repository.UserRepository;
+import user.User;
+
+public class UserManager {
+  public UserRepository userRepository;
+
+  public UserManager(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
+
+  public boolean areCredentialsValid(User user) {
+    if (user == null) return false;
+    User storedUser = userRepository.findByLogin(user.getLogin());
+    return storedUser != null && verify(user.getPassword(), storedUser.getPassword());
+  }
+
+  private boolean verify(byte[] rawPass, byte[] encrypted) {
+    return BCrypt.verifyer().verify(rawPass, encrypted).verified;
+  }
+
+  public boolean isUserUnique(User user) {
+    if (user == null) return false;
+    return (null == userRepository.findByLogin(user.getLogin()));
+  }
+
+  public long save(User user) {
+    byte[] encryptedPass = BCrypt.withDefaults().hash(12, user.getPassword());
+    user.setPassword(encryptedPass);
+    long id = userRepository.save(user);
+    user.setId(id);
+    return id;
+  }
+}
