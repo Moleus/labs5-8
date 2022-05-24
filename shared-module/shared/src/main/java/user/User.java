c1c57021b2a60004d7fb24899e404ec4d1da9c09
Moleus <fafufuburr@gmail.com>
2022-05-24 13:29:50 +0300

    [Fix] Add necessary constructors to User class.

--- shared-module/shared/src/main/java/user/User.java
+++ shared-module/shared/src/main/java/user/User.java
@@ -1,6 +1,8 @@
 package user;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import perform.annotations.*;
 
 import java.io.Serializable;
@@ -8,14 +10,20 @@ import java.io.Serializable;
 @Data
 @Entity
 @Table(name = "users")
+@NoArgsConstructor
 public class User implements Serializable {
   @Id
   private long id;
 
   @NotNull
   @Unique
-  private final String login;
+  private String login;
 
   @NotNull
-  private final byte[] password;
+  private byte[] password;
+
+  public User(String login, byte[] password) {
+    this.login = login;
+    this.password = password;
+  }
 }
