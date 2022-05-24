1bf522d33c1183bd327d4bc6d6e6525ba03de5d8
Moleus <fafufuburr@gmail.com>
2022-05-24 14:17:21 +0300

    [Fix] ClientContext is not Parcelable. RawValue is only for Android.

--- client-ui/domain/build.gradle.kts
+++ client-ui/domain/build.gradle.kts
@@ -6,13 +6,14 @@ kotlin {
     sourceSets {
         named("commonMain") {
             dependencies {
-                implementation("org.jline:jline:3.21.0")
                 implementation(project(Module.common))
                 implementation(project(Module.Feature.auth))
 
                 implementation(Module.Shared.shared)
                 implementation(Module.Shared.annotation_processor)
                 implementation(Module.Shared.perform)
+
+                implementation(Libs.ArkIvanov.Decompose.common)
             }
         }
     }
--- client-ui/feature-root/src/commonMain/kotlin/ru.moleus.kollector/feature/root/RootComponent.kt
+++ client-ui/feature-root/src/commonMain/kotlin/ru.moleus.kollector/feature/root/RootComponent.kt
@@ -9,7 +9,6 @@ import com.arkivanov.essenty.parcelable.Parcelable
 import com.arkivanov.essenty.parcelable.Parcelize
 import common.context.ClientContext
 import common.context.Session
-import kotlinx.android.parcel.RawValue
 import ru.moleus.kollector.feature.connection.Connection
 import ru.moleus.kollector.feature.connection.integration.ConnectionComponent
 import ru.moleus.kollector.feature.main.Main
@@ -20,6 +19,7 @@ class RootComponent(
     private val clientSession: Session,
     private val getContext: (Session) -> ClientContext
 ) : Root, ComponentContext by componentContext {
+    private lateinit var clientContext: ClientContext
     private val router =
         router<Config, Root.Child>(
             initialConfiguration = Config.Connection,
@@ -29,26 +29,28 @@ class RootComponent(
 
     override val routerState: Value<RouterState<*, Root.Child>> = router.state
 
+
     private fun createChild(config: Config, componentContext: ComponentContext): Root.Child =
         when (config) {
             is Config.Connection -> Root.Child.Connection(connection(componentContext))
-            is Config.Main -> Root.Child.MainChild(main(componentContext, config.clientContext))
+            is Config.Main -> Root.Child.MainChild(main(componentContext))
             else -> Root.Child.Connection(connection(componentContext))
         }
 
-    private fun main(componentContext: ComponentContext, clientContext: ClientContext): Main =
+    private fun main(componentContext: ComponentContext): Main =
         MainComponent(componentContext, clientContext)
 
     private fun connection(componentContext: ComponentContext): Connection =
         ConnectionComponent(componentContext, clientSession, getContext, ::onConnected)
 
     private fun onConnected(clientContext: ClientContext) {
-        router.replaceCurrent(Config.Main(clientContext))
+        this.clientContext = clientContext
+        router.replaceCurrent(Config.Main)
     }
 
     interface Config : Parcelable {
         @Parcelize
-        data class Main(val clientContext: @RawValue ClientContext) : Config
+        object Main : Config
 
         @Parcelize
         object Connection : Config
