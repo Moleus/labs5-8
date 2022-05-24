5370cf1c7b4873b0540ea2295c23e90f70e62412
Moleus <fafufuburr@gmail.com>
2022-05-24 14:00:18 +0300

    [Change] Use enum instead of hard-coded names.

---
+++ client-ui/data-local/src/commonMain/kotlin/ru/moleus/kollector/data/local/model/table/HeaderLabel.kt
@@ -0,0 +1,18 @@
+package ru.moleus.kollector.data.local.model.table
+
+enum class HeaderLabel(val eng: String) {
+    ID("Id"),
+    NAME("Name"),
+    CORD_X("Cord. X"),
+    CORD_Y("Cord. Y"),
+    CREATION_DATE("Creation Date"),
+    AREA("Area"),
+    ROOMS("Rooms"),
+    FURNITURE("Furniture"),
+    NEWNESS("Newness"),
+    VIEW("View"),
+    HOUSE_NAME("House name"),
+    HOUSE_YEAR("House year"),
+    HOUSE_FLOORS("House floors"),
+    HOUSE_LIFT("House lifts")
+}
